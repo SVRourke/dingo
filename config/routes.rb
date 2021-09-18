4bd813d3d33c9614309a8cf2f0325b12c1bd8f27
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-18 16:11:01 -0400

    added routes for logging in and out, messing with routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'login', to: 'auth#new'
+  post 'login', to: 'auth#create'
+  delete 'logout', to: 'auth#destroy'
+
+  resource :user do
+    get 'images', to: "images#index"
+  end
+  resource :images, only: [:create, :show, :index, :update] 
+
 end
