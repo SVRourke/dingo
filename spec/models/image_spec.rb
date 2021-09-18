f49274802b5f90f879ad049d285f2715c69addd6
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-18 14:18:55 -0400

    testing image-tag relationship

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Image, type: :model do
     puts User.first
     image = Image.create
   end
+
+  it "has many tags" do
+    user = User.create(username: "SVROURKE", password: "password")
+    img = user.images.create(caption: "vlbajofjsf")
+    tag = user.tags.create(name: "outdoors")
+    img.tags.push(tag)
+    expect(img.tags.count).to eql 1
+  end
   # pending "add some examples to check attached file #{__FILE__}"
 end
