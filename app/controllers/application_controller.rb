17c1bc72596fb584c6b3895eece7b3063f4a8da3
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-18 16:09:56 -0400

    added methods for checking logged in state retreiving the logged in user and redirecting based on logged in state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    def authorized_redirect
+        puts "HELOO"
+        redirect_to :images_user_path if !logged_in?
+    end
+
+    def logged_in?
+        session[:user_id].present? && current_user.id == session[:user_id]
+    end
+
+    def current_user
+        @user ||= User.find(session[:user_id])
+    end
 end
