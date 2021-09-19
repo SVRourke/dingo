7f6c190169cf3a3f6a8d91db5c9d53ef02248a8a
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-18 23:35:53 -0400

    created image integration test

---
+++ spec/requests/images_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Images", type: :request do
+  context "create image" do
+    # valid info can create an image
+  end
+
+  context "tags" do
+    it "can add a tag" do
+      @user = User.create(username: "sam", password: "password")
+      post login_path, params: {username: "sam", password: "password"}
+
+      @image = @user.images.create(caption: "blah blahblah")
+      @tag = @user.tags.create(name: "Outdoors")
+      
+      post add_user_image_tag_path(@user, @image, @tag)
+      expect(@image.tags.length).to eql 1
+      
+    end
+    
+    it "can remove tag" do
+      @user = User.create(username: "sam", password: "password")
+      post login_path, params: {username: "sam", password: "password"}
+      
+      @image = @user.images.create(caption: "blah blahblah")
+      @tag = @user.tags.create(name: "Outdoors")
+
+     @image.tags.push(@tag)
+     byebug
+      
+      post remove_user_image_tag_path(@user, @image, @tag)
+      expect(@image.tags.length).to eql 0
+      
+    end
+    
+  end
+
+end
