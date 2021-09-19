6785dd13bf71a4980846e629aa03bb030ffdc6db
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-19 13:45:57 -0400

    redirecting logout to login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,24 +1,24 @@
 class AuthController < ApplicationController
-    def def new
-    end
-    
-    def create
-        @user = User.find_by(username: params[:username])
-        
-        if @user.authenticate(params[:password])
-          flash[:success] = "logged in"
-          session[:user_id] = @user.id
-          redirect_to user_image_index_path(@user)
-        else
-          flash[:error] = "Something went wrong"
-          redirect_to login_path
-        end
-    end
-    
-    def destroy
-        if logged_in?
-            session.clear()
-        end
-    end
+  def new
+  end
 
+  def create
+    @user = User.find_by(username: params[:username])
+
+    if @user.authenticate(params[:password])
+      flash[:success] = "logged in"
+      session[:user_id] = @user.id
+      redirect_to user_image_index_path(@user)
+    else
+      flash[:error] = "Something went wrong"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    if logged_in?
+      session.clear()
+      redirect_to :login
+    end
+  end
 end
