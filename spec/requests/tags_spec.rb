977ac25abe70affed18835a07de47e7fde2c3ca9
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-18 23:36:16 -0400

    testing if a valid tag can be created

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -11,12 +11,18 @@ RSpec.describe "Tags", type: :request do
       
       expect(response).to be_successful   
     end
-    
   end
 
   context "create a tag" do
-    
+    it "valid info is successful" do
+      @user = User.create(username: "sam", password: "password")
+      post login_path, params: {username: "sam", password: "password"}
+      expect(session[:user_id]).to eql @user.id
+      post user_tags_path(@user), params: {tag: {name: "outdoors"}}
+      expect(@user.tags.length).to eql 1
+    end
   end
+
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
