422a13b4a6ec3f741829de76812cf55ae0838781
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-19 12:34:53 -0400

    formatting testing deletion

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,27 +1,29 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-
   context "can create user" do
     it "POST: users_path with valid info creates a user" do
-      post users_path, params: {user: {username: "sam", password: "password1234"}}
+      post users_path, params: { user: { username: "sam", password: "password1234" } }
       expect(response.status).to eql 302
     end
-    
+
     it "POST: users_path with valid info redirects to image index" do
-      post users_path, params: {user: {username: "sam", password: "password1234"}}
+      post users_path, params: { user: { username: "sam", password: "password1234" } }
       expect(response).to redirect_to(user_image_index_path(User.first))
     end
-    
+
     it "creating a user logs the user in" do
-      post users_path, params: {user: {username: "sam", password: "password1234"}}
+      post users_path, params: { user: { username: "sam", password: "password1234" } }
       expect(session[:user_id]).to eql 1
     end
   end
 
   context "can delete a user" do
     it "DELETE: users_path deletes a user" do
-      # TODO
+      @user = User.create(username: "sam", password: "password")
+      post login_path, params: { username: "sam", password: "password" }
+      delete user_path(@user)
+      expect(User.count).to eql 0
     end
   end
 end
