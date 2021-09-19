de318415bf455efbff3bb5c2c8bd9abcb7caed24
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-19 09:28:13 -0400

    moved tag tests to tag spec

--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -5,32 +5,6 @@ RSpec.describe "Images", type: :request do
     # valid info can create an image
   end
 
-  context "tags" do
-    it "can add a tag" do
-      @user = User.create(username: "sam", password: "password")
-      post login_path, params: {username: "sam", password: "password"}
-
-      @image = @user.images.create(caption: "blah blahblah")
-      @tag = @user.tags.create(name: "Outdoors")
-      
-      post add_user_image_tag_path(@user, @image, @tag)
-      expect(@image.tags.length).to eql 1
-      
-    end
-    
-    it "can remove tag" do
-      @user = User.create(username: "sam", password: "password")
-      post login_path, params: {username: "sam", password: "password"}
-      
-      @image = @user.images.create(caption: "blah blahblah")
-      @tag = @user.tags.create(name: "Outdoors")
-
-     @image.tags.push(@tag)
-     post remove_user_image_tag_path(@user.id, @image.id, @tag.id)
-     expect(@image.tags.length).to eq; 0
-      
-    end
-    
-  end
+  
 
 end
