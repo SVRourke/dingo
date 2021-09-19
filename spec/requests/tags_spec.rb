0e7c9dda7d1336019f11618915aabf92849e587f
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-19 09:27:45 -0400

    moved adding and removing a tag from an image to tag requests spec

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -17,13 +17,40 @@ RSpec.describe "Tags", type: :request do
     it "valid info is successful" do
       @user = User.create(username: "sam", password: "password")
       post login_path, params: {username: "sam", password: "password"}
+      
       expect(session[:user_id]).to eql @user.id
       post user_tags_path(@user), params: {tag: {name: "outdoors"}}
       expect(@user.tags.length).to eql 1
     end
   end
 
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "image" do
+    it "can add a tag" do
+      @user = User.create(username: "sam", password: "password")
+      post login_path, params: {username: "sam", password: "password"}
+
+      @image = @user.images.create(caption: "blah blahblah")
+      @tag = @user.tags.create(name: "Outdoors")
+      
+      post add_user_image_tag_path(@user, @image, @tag)
+      expect(@image.tags.length).to eql 1
+      
+    end
+    
+    it "can remove tag" do
+      @user = User.create(username: "sam", password: "password")
+      post login_path, params: {username: "sam", password: "password"}
+      
+      @image = @user.images.create(caption: "blah blahblah")
+      @tag = @user.tags.create(name: "Outdoors")
+      @image.tags.push(@tag)
+
+     post remove_user_image_tag_path(@user.id, @image.id, @tag.id)
+
+     @updated_image = Image.find(@image.id)
+
+     expect(@updated_image.tags.length).to eq 0
+      
+    end
   end
 end
