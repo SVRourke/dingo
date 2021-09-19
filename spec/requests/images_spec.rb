0ed6ace328528e1b587e92edd0a2dd556d432e04
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-19 12:34:16 -0400

    testing for valid creation, adding tags on creation, as well as show and deletion capabilities

--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -1,10 +1,36 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Images", type: :request do
-  context "create image" do
-    # valid info can create an image
+  before(:each) do
+    @user = User.create(username: "sam", password: "password")
+    @tag = @user.tags.create(name: "outdoors")
+    post login_path, params: { username: "sam", password: "password" }
+    expect(session[:user_id]).to eql @user.id
   end
 
-  
+  context "create image" do
+    it "can create a valid image" do
+      post user_image_index_path(@user), params: { image: { caption: "blah blah blah" } }
+      @image = Image.find_by(caption: "blah blah blah")
+      expect(@image.valid?).to be true
+    end
 
+    it "adds tags when provided as part of the request" do
+      post user_image_index_path(@user), params: { image: { caption: "blah blah blah", tag_ids: [@tag.id] } }
+      @image = Image.find_by(caption: "blah blah blah")
+      expect(@image.tags.first.id).to be @tag.id
+    end
+  end
+
+  it "can delete images" do
+    @image = @user.images.create(caption: "hello")
+    delete user_image_path(@user, @image)
+    expect(Image.all.count).to eql 0
+  end
+
+  it "can show images" do
+    @image = @user.images.create(caption: "hello")
+    get user_image_path(@user, @image)
+    expect(response).to render_template(:show)
+  end
 end
