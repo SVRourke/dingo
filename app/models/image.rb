a5144801da10fa96f4aca27e9ae25446498e2675
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-19 14:40:33 -0400

    establish image file attachment to image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
-    belongs_to :user  
-    has_and_belongs_to_many :tags
+  belongs_to :user
+  has_and_belongs_to_many :tags
+
+  has_one_attached :image_file
 end
