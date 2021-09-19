2c7c038248db2b3d253c7123a59f93b6b0e2b294
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-19 12:30:26 -0400

    add user to show for layout, finish destroy

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,32 +1,38 @@
 class ImageController < ApplicationController
-    def index
-        @user = current_user()
-        @images = @user.images
+  def index
+    @user = current_user()
+    @images = @user.images
+  end
+
+  def new
+    @user = current_user
+    @image = Image.new
+    @tags = @user.tags
+  end
+
+  def create
+    @image = current_user.images.create(image_params)
+    if @image.valid?
+      redirect_to user_image_index_path(current_user)
+    else
+      redirect_to :back
     end
+  end
 
-    def new
-        @user = current_user
-        @image = Image.new
-        @tags = @user.tags
-    end
+  def show
+    @user = current_user
+    @image = Image.find(params[:id])
+  end
 
-    def create
-        @image = current_user.images.create(image_params)
-        if @image.valid?
-            redirect_to user_image_index_path(current_user)
-        else
-            redirect_to :back
-        end
-    end
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to user_image_index_path(current_user)
+  end
 
-    def show
-        @image = Image.find(params[:id])
-    end
+  private
 
-    private
-
-    def image_params
-        params.require(:image).permit(:caption, tag_ids: [])
-    end
-
+  def image_params
+    params.require(:image).permit(:caption, tag_ids: [])
+  end
 end
