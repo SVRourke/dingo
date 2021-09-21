837bd85ba0a3e8945217bfecb3f47ef436e11d09
SVRourke <samuelvanrourke@gmail.com>
2021-09-21 12:07:42 -0400

    created image policy spec

---
+++ spec/policies/image_policy_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe ImagePolicy do
+    subject {ImagePolicy.new(user, article)}
+
+    
+    # context "not signed in" do
+    #     let(:user) {nil}
+    #     it {should_not permit(:show)}
+    # end
+end
