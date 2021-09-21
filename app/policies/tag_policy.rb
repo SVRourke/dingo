b7cb14beb6c75fb621beed9608d3ada9cc50efdd
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-21 17:23:14 -0400

    refactored policy

--- app/policies/tag_policy.rb
+++ app/policies/tag_policy.rb
@@ -1,25 +1,25 @@
 class TagPolicy < ApplicationPolicy
-    def index?
-        user.present?
-    end
+  def index?
+    @user.present? && @user == @record
+  end
 
-    def create?
-        @user.present?
-    end
-    
-    def show?
-        @user.present? && @record.user == @user
-    end
-    
-    def destroy?
-        @user.present? && @record.user == @user
-    end
-    
-    def add?
-        @user.present? && @record.user == @user
-    end
-    
-    def remove?
-        @user.present? && @record.user == @user
-    end
+  def create?
+    @user.present? && @user.id == @record
+  end
+
+  def show?
+    @user.present? && @record.user == @user
+  end
+
+  def destroy?
+    @user.present? && @record.user == @user
+  end
+
+  def add?
+    @user.present? && @record.user == @user
+  end
+
+  def remove?
+    @user.present? && @record.user == @user
+  end
 end
--- spec/policies/image_policy_spec.rb
+++ spec/policies/image_policy_spec.rb
@@ -1,11 +1,33 @@
 require "spec_helper"
+require "rails_helper"
 
 describe ImagePolicy do
-    subject {ImagePolicy.new(user, article)}
+  subject { ImagePolicy }
 
-    
-    # context "not signed in" do
-    #     let(:user) {nil}
-    #     it {should_not permit(:show)}
-    # end
+  before(:each) do
+    @tony = User.create(username: "TonySoprano", password: "password")
+    @junior = User.create(username: "JuniorSoprano", password: "password")
+    @image = @tony.images.create(caption: "money")
+  end
+
+  permissions :show? do
+    it "denies access if a user did not upload" do
+      expect(subject).not_to permit(@junior, @image)
+    end
+  end
+  permissions :destroy? do
+    it "only the uploader may delete an image" do
+      expect(subject).not_to permit(@junior, @image)
+    end
+  end
+  permissions :index? do
+    it "a user can only view their own files" do
+      expect(subject).not_to permit(@junior, @tony.images)
+    end
+  end
+  permissions :create? do
+    it "a user can only upload to their own account" do
+      expect(subject).not_to permit(@junior, @tony)
+    end
+  end
 end
