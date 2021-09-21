64e5e00becded76fc876707dd0ae7395db38d00e
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-21 17:22:56 -0400

    refactored image policy

--- app/policies/image_policy.rb
+++ app/policies/image_policy.rb
@@ -1,21 +1,21 @@
 class ImagePolicy < ApplicationPolicy
-    def index?
-        @user.present?
-    end
-    
-    def new?
-        @user.present?
-    end
+  def index?
+    @user.images == @images
+  end
 
-    def create?
-        @user.present?
-    end
-    
-    def show?
-        @user.present? && @record.user == @user
-    end
-    
-    def destroy?
-        @user.present? && @record.user == @user
-    end
+  def new?
+    @user.present?
+  end
+
+  def create?
+    @user.id == @record.id
+  end
+
+  def show?
+    @user.present? && @record.user == @user
+  end
+
+  def destroy?
+    @user.present? && @record.user == @user
+  end
 end
